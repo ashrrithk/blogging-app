0047ac8794ae60faa807626522151a635326c47b
ashrrithk <ashrrithkarunaakar@gmail.com>
2024-05-02 20:16:09 +0530

    frontend added signin/up

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,15 @@
+export const Quote = () => {
+  return (
+    <div className="bg-[#FFC016] h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className="text-3xl font-bold">
+            "When everything seems to be going against you, remember that the
+            airplane takes off against the wind, not with it"
+          </div>
+          <div className="max-w-md text-xl font-semibold mt-2">Henry Ford</div>
+        </div>
+      </div>
+    </div>
+  );
+};
